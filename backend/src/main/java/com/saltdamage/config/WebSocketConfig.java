b0148f222a53c4d128f9957f9a3ac817e44ee204
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-12 12:54:58 +0800

    Update project

---
+++ backend/src/main/java/com/saltdamage/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.saltdamage.config;
+
+import com.saltdamage.web.WebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/monitor")
+                .setAllowedOriginPatterns("*");
+    }
+}
