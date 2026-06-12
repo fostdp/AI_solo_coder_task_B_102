fe1d04f61a58b055a4d73b4ad0acea43badfb2c4
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-12 12:53:45 +0800

    Update project

---
+++ backend/flaking-rf/src/main/java/com/saltdamage/flaking/FlakingRfApplication.java
@@ -0,0 +1,12 @@
+package com.saltdamage.flaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlakingRfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlakingRfApplication.class, args);
+    }
+}
