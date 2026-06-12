70012e6dd19b98ca7221519a3f48cc2313c141e4
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-12 12:53:30 +0800

    Update project

---
+++ backend/dqn-controller/src/main/java/com/saltdamage/dqn/dto/EnvironmentStateDTO.java
@@ -0,0 +1,23 @@
+package com.saltdamage.dqn.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class EnvironmentStateDTO {
+
+    private BigDecimal currentRh;
+
+    private BigDecimal rhTrend;
+
+    private Integer currentHour;
+
+    private Boolean dehumidifierStatus;
+
+    private Boolean humidifierStatus;
+
+    private Integer recommendedAction;
+
+    private BigDecimal expectedReward;
+}
