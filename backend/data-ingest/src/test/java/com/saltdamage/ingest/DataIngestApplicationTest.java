a9dceede432464c5b47a454c2f62260de4443b89
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-12 12:53:20 +0800

    Update project

---
+++ backend/data-ingest/src/test/java/com/saltdamage/ingest/DataIngestApplicationTest.java
@@ -0,0 +1,12 @@
+package com.saltdamage.ingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataIngestApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
